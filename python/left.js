807066c88aeab84e2d998bdd8d9a94543bea40f1
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-04-26 18:48:12 +0900

    Add files via upload

--- python/left.js
+++ python/left.js
@@ -1,4 +1,6 @@
+document.write("<ul id='left'>");
 var pythons = {"gif 만들기":"gif maker","GUI 연습":"gui practice","로그인 연습":"login practice"};
 for (var i in pythons){
     document.write("<li><a href='./"+pythons[i]+".html'>"+i+"</a></li>")
 }
+document.write("</ul>");
