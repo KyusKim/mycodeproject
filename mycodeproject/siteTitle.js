889a395e14743765e9e67869aefa4fd4724aa319
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-04-26 16:45:23 +0900

    첫 시험적 업로드

---
+++ mycodeproject/siteTitle.js
@@ -0,0 +1,4 @@
+(function (){
+    var sitename = "Kyus's Codes";
+    document.write("<h1 style='text-align:center'><a href='/mycodeproject/main.html' style='text-decoration-line:none;color=black'>"+sitename+"</a></h1>");
+})();
