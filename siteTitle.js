59cb1673c2e51b4675a3bbe72d9fe012f8ec3e5b
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-04-26 18:36:32 +0900

    Update siteTitle.js

--- siteTitle.js
+++ siteTitle.js
@@ -1,4 +1,4 @@
 (function (){
     var sitename = "Kyus's Codes";
-    document.write("<h1 style='text-align:center'><a href='https://kyuskim.github.io/mycodeproject/index.html' style='text-decoration-line:none;color=black'>"+sitename+"</a></h1>");
+    document.write("<h1 style='text-align:center'><a href='https://kyuskim.github.io/mycodeproject' style='text-decoration-line:none;color=black'>"+sitename+"</a></h1>");
 })();
