8807fe7af69d311c99fc10b9a3efdd98bda903e6
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-04-27 14:41:40 +0900

    Delete mycodeproject directory

--- mycodeproject/java/left.js
+++
@@ -1,6 +0,0 @@
-document.write("<ul id='left'>");
-var pythons = {"심플 블로그":"simpleblog"};
-for (var i in pythons){
-    document.write("<li><a href='./"+pythons[i]+".html'>"+i+"</a></li>");
-}
-document.write("</ul>");
--- mycodeproject/python/left.js
+++
@@ -1,8 +0,0 @@
-document.write("<ul id='left'>");
-var pythons = {"개인 관리 어플":"personal_manage","GUI 연습":"gui practice"};
-for (var i in pythons){
-    if (window.location.pathname.indexOf(pythons[i])>=0) {
-        document.write("<li id='selected'><a href='./"+pythons[i]+".html'>"+i+"</a></li>");}
-    else{document.write("<li><a href='./"+pythons[i]+".html'>"+i+"</a></li>");}
-}
-document.write("</ul>");
