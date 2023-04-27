ce7ab7e3e4ef9c455338506b28bffb5991b1a73b
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-04-27 14:42:13 +0900

    Add files via upload

--- java/left.js
+++ java/left.js
@@ -1,5 +1,5 @@
 document.write("<ul id='left'>");
-var pythons = {"심플 블로그":"simpleblog","한/컴 타자 연습":"typing practice","가사 출력기":"lyrics practice"};
+var pythons = {"심플 블로그":"simpleblog"};
 for (var i in pythons){
     document.write("<li><a href='./"+pythons[i]+".html'>"+i+"</a></li>");
 }
