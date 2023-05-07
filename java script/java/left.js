b1f48b419d2b2ef8b865d0c86b51417b939a6d0a
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-05-07 15:28:29 +0900

    Delete java script directory

--- java script/java/left.js
+++
@@ -1,8 +0,0 @@
-document.write("<ul id='left'>");
-var contents = {"심플 블로그":"simpleblog"};
-for (var i in contents){
-    if (window.location.pathname.indexOf(contents[i])>=0) {
-        document.write("<li id='selected'><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
-    else{document.write("<li><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
-}
-document.write("</ul>");
--- java script/left.js
+++
@@ -1,8 +0,0 @@
-document.write("<ul id='left'>");
-var contents = {"개인 웹페이지 만들기":"personal_site"};
-for (var i in contents){
-    if (window.location.pathname.indexOf(contents[i])>=0) {
-        document.write("<li id='selected'><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
-    else{document.write("<li><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
-}
-document.write("</ul>");
