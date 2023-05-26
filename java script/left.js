014be87633ae478ca84157ed6eba5b1542f07f80
KyusKim <131855669+KyusKim@users.noreply.github.com>
2023-05-26 12:44:04 +0900

    Add files via upload

--- java script/left.js
+++ java script/left.js
@@ -1,5 +1,5 @@
 document.write("<ul id='left'>");
-var contents = {"개인 웹페이지 만들기":"personal_site","html 만들기":"html_maker","naver 로그인":"naver","p5 단순 타이밍 게임":"p5"};
+var contents = {"개인 웹페이지 만들기":"personal_site","html 만들기":"html_maker","naver 로그인":"naver","p5 단순 타이밍 게임":"p5","p5 리듬게임":"p5rhythm"};
 for (var i in contents){
     if (window.location.pathname.indexOf(contents[i])>=0) {
         document.write("<li id='selected'><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
--- java/left.js
+++ java/left.js
@@ -1,5 +1,5 @@
 document.write("<ul id='left'>");
-var contents = {"심플 블로그":"simpleblog"};
+var contents = {"심플 블로그":"simpleblog","노트 생성기":"notemaker"};
 for (var i in contents){
     if (window.location.pathname.indexOf(contents[i])>=0) {
         document.write("<li id='selected'><a href='./"+contents[i]+".html'>"+i+"</a></li>");}
